adfccfa819f97f617cfebb53a88d0db58cc75b66
wjbaker101 <willbaker10198@gmail.com>
2021-08-19 18:18:06 +0100

    Update references to the mod version

--- src/main/java/com/wjbaker/ccm/CustomCrosshairMod.java
+++ src/main/java/com/wjbaker/ccm/CustomCrosshairMod.java
@@ -35,7 +35,7 @@ public final class CustomCrosshairMod {
 
     public static final String TITLE = "Custom Crosshair Mod";
     public static final String VERSION = "1.3.4-forge";
-    public static final String MC_VERSION = "1.16.5-forge";
+    public static final String MC_VERSION = "1.17.1-forge";
     public static final String CURSEFORGE_PAGE = "https://www.curseforge.com/projects/242995/";
     public static final String MC_FORUMS_PAGE = "https://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/2637819/";
 
