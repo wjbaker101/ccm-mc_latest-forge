20e646e8d166e79a95e298809eade3c1efc4aa61
wjbaker101 <willbaker10198@gmail.com>
2021-12-19 15:03:55 +0000

    Add Minecraft as a property so it can be used throughout

--- src/main/java/com/wjbaker/ccm/crosshair/style/AbstractCrosshairStyle.java
+++ src/main/java/com/wjbaker/ccm/crosshair/style/AbstractCrosshairStyle.java
@@ -2,14 +2,17 @@ package com.wjbaker.ccm.crosshair.style;
 
 import com.wjbaker.ccm.crosshair.CustomCrosshair;
 import com.wjbaker.ccm.render.RenderManager;
+import net.minecraft.client.Minecraft;
 
 public abstract class AbstractCrosshairStyle implements ICrosshairStyle {
 
     protected final CustomCrosshair crosshair;
     protected final RenderManager renderManager;
+    protected final Minecraft mc;
 
     public AbstractCrosshairStyle(final CustomCrosshair crosshair) {
         this.crosshair = crosshair;
         this.renderManager = new RenderManager();
+        this.mc = Minecraft.getInstance();
     }
 }
