cd9898f6b3fe3f6942db5edd6f4a34c6c4dce642
William Baker <wjbaker101@users.noreply.github.com>
2024-04-11 21:08:56 +0100

    Use translations for the drawCrosshair screen

--- src/main/java/com/wjbaker/ccm/gui/screen/screens/drawCrosshair/DrawCrosshairGuiScreen.java
+++ src/main/java/com/wjbaker/ccm/gui/screen/screens/drawCrosshair/DrawCrosshairGuiScreen.java
@@ -2,25 +2,26 @@ package com.wjbaker.ccm.gui.screen.screens.drawCrosshair;
 
 import com.wjbaker.ccm.CustomCrosshairMod;
 import com.wjbaker.ccm.crosshair.properties.IntegerProperty;
-import com.wjbaker.ccm.gui.component.components.PanelGuiComponent;
-import com.wjbaker.ccm.gui.component.event.IOnClickEvent;
-import com.wjbaker.ccm.gui.screen.screens.drawCrosshair.components.DrawCrosshairGuiComponent;
 import com.wjbaker.ccm.gui.component.components.ButtonGuiComponent;
 import com.wjbaker.ccm.gui.component.components.HeadingGuiComponent;
+import com.wjbaker.ccm.gui.component.components.PanelGuiComponent;
+import com.wjbaker.ccm.gui.component.event.IOnClickEvent;
 import com.wjbaker.ccm.gui.screen.GuiScreen;
+import com.wjbaker.ccm.gui.screen.screens.drawCrosshair.components.DrawCrosshairGuiComponent;
+import net.minecraft.client.resources.language.I18n;
 
 public final class DrawCrosshairGuiScreen extends GuiScreen {
 
     public DrawCrosshairGuiScreen() {
         super("Draw Crosshair");
 
-        var titleHeading = new HeadingGuiComponent(this, -1, -1, "Draw Crosshair");
+        var titleHeading = new HeadingGuiComponent(this, -1, -1, I18n.get("custom_crosshair_mod.screen.draw_crosshair.heading"));
 
         var customCrosshairDrawer = CustomCrosshairMod.INSTANCE.properties().getCustomCrosshairDrawer();
         var imageSize = new IntegerProperty("fake_image_size", customCrosshairDrawer.getWidth());
         var drawCrosshair = new DrawCrosshairGuiComponent(this, -1, -1, imageSize);
 
-        var resetButton = new ButtonGuiComponent(this, -1, -1, 50, 15, "Reset");
+        var resetButton = new ButtonGuiComponent(this, -1, -1, 50, 15, I18n.get("custom_crosshair_mod.screen.draw_crosshair.reset"));
         resetButton.addEvent(
             IOnClickEvent.class,
             () -> customCrosshairDrawer.reset(customCrosshairDrawer.getWidth(), customCrosshairDrawer.getHeight()));
